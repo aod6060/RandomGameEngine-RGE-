ec9048f473152991ae18199a8918697078bf9cac
aod6060 <a.o.d.6060@gmail.com>
2016-05-31 18:46:00 -0500

    First Video

---
+++ src/sys.h
@@ -0,0 +1,26 @@
+#ifndef SYS_H
+#define SYS_H
+
+
+
+// Standard Libraries
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <ctime>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <map>
+// 3rd Party
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_ttf.h>
+#include <GL/glew.h>
+#include <glm/glm.hpp>
+#include <glm/ext.hpp>
+// Project Libries 
+
+#endif
